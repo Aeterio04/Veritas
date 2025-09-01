6e3ff8b6fe664abd03b598b26dd3b1ba87238d52
Aeterio04 <ojsangwai17@gmail.com>
2025-09-01 16:33:59 +0530

    created the first migration data and linked with Psql

--- veritas/veritas/settings.py
+++ veritas/veritas/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'veritas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'veritasdb',       # database name
+        'USER': 'veritasuser',     # created user
+        'PASSWORD': 'veritaspass',    # user password
+        'HOST': 'localhost',         # use IP/domain if remote
+        'PORT': '5432',              # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
