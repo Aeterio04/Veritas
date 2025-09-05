154eda4fda03c8041106bebd578ec477e6ce9fae
Aeterio04 <ojsangwai17@gmail.com>
2025-09-05 23:17:47 +0530

    added auther

--- veritas/auther/views.py
+++ veritas/auther/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+from django.contrib.auth import authenticate,logout
+from django.utils.text import slugify
+from django.contrib.auth.hashers import check_password
+from datetime import datetime
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate, login
+from rest_framework.authtoken.models import Token
+from datetime import date,timedelta
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import EmailMessage
 
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def loginfunc(request):
 
-def hello():
-    print("Hello, World!")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    print(email,password)
+    return JsonResponse({"status":"success","message":"Login successful"})
+
 # Create your views here.
